ef192ebcc8fbf9e61b315b34bbded76472c65721
Burkov Egor <xwooffie@gmail.com>
2023-04-06 19:34:23 +0300

    #4 Chain documentation

--- src/chain.rs
+++ src/chain.rs
@@ -5,14 +5,30 @@ use serde::{Deserialize, Serialize};
 
 use crate::Block;
 
+/// A blockchain that consists of a vector of `Block`s and maintains a queue of `Block`s yet to be
+/// appended to the chain.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Chain {
+    /// The vector of blocks that make up the blockchain.
     pub blocks: Vec<Block>,
+    /// The current status of the blockchain. True - can get new blocks. False - calculating hash of last block yet.
     pub status: bool,
+    /// The queue of blocks that are yet to be appended to the blockchain.
     pub queue: VecDeque<Block>,
 }
 
 impl Chain {
+    /// Constructs a new, empty blockchain `Chain`.
+    ///
+    /// This function creates a new, empty `Chain` instance with no blocks or transactions.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rustychain::Chain;
+    ///
+    /// let chain = Chain::new();
+    /// ```
     pub fn new() -> Self {
         Chain {
             blocks: vec![],
@@ -21,6 +37,30 @@ impl Chain {
         }
     }
 
+    /// Checks if the chain contains any errors.
+    ///
+    /// Returns `None` if the chain is valid, or the index of the first invalid block
+    /// encountered in the chain.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rustychain::Chain;
+    /// use rustychain::Block;
+    ///
+    /// let mut chain = Chain::new();
+    ///
+    /// assert_eq!(chain.have_errors(), None);
+    ///
+    /// let block1 = Block::new(0,"Some data".to_owned());
+    /// let block2 = Block::new(1,"Some data".to_owned());
+    /// chain.status = false;
+    /// chain.blocks.push(block1);
+    /// assert_eq!(chain.have_errors(), None);
+    /// chain.blocks.push(block2);
+    /// assert_eq!(chain.have_errors(), Some(0));
+    ///
+    /// ```
     pub fn have_errors(&self) -> Option<usize> {
         let mut len = self.blocks.len();
         if len == 0 {
@@ -52,6 +92,33 @@ impl Chain {
         None
     }
 
+    /// Attempts to add a new block to the chain. If the chain is currently in an invalid state,
+    /// this function will return false.
+    ///
+    /// Returns true if the new block is added to the chain, false otherwise.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rustychain::Chain;
+    /// use rustychain::Block;
+    ///
+    /// let mut chain = Chain::new();
+    /// let block = Block::new(1, "data1".to_string());
+    ///
+    /// chain.queue.push_back(block);
+    ///
+    /// assert_eq!(chain.try_add(), true);
+    /// assert_eq!(chain.try_add(), false);
+    /// assert_eq!(chain.blocks.len(), 1);
+    ///
+    /// let block2 = Block::new(2, "data2".to_string());
+    ///
+    /// chain.queue.push_back(block2);
+    ///
+    /// assert_eq!(chain.try_add(), true);
+    /// assert_eq!(chain.blocks.len(), 2);
+    /// ```
     pub fn try_add(&mut self) -> bool {
         if self.status {
             // ready
@@ -74,6 +141,27 @@ impl Chain {
         }
     }
 
+    /// Adds a new block to the end of the queue.
+    ///
+    /// The ID of the block will be set to the sum of the number of blocks in the chain
+    /// and the number of blocks in the queue. The block will be added to the back of the
+    /// queue, waiting to be added to the chain by calling the `try_add()` method.
+    ///
+    /// # Arguments
+    ///
+    /// * `block` - A `Block` instance representing the block to be added to the queue.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustychain::Chain;
+    /// use rustychain::Block;
+    ///
+    /// let mut chain = Chain::new();
+    /// let block = Block::new(1, "First Block".to_owned());
+    /// chain.add_queue(block);
+    /// assert_eq!(chain.queue.len(), 1);
+    /// ```
     pub fn add_queue(&mut self, mut block: Block) {
         block.id = (self.blocks.len() + self.queue.len()) as u64;
         self.queue.push_back(block);
